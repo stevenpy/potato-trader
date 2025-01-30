b5e5f1ac184624edfe09c3beee178edb0df1be50
stevenpy <stevenphilbert@gmail.com>
2025-01-30 18:02:28 +0100

    Update ProfitController

--- app/controllers/api/v1/profits_controller.rb
+++ app/controllers/api/v1/profits_controller.rb
@@ -3,15 +3,15 @@ module Api
     class ProfitsController < ApplicationController
       def max_potential_profit
         date = parse_and_validate_date
-        max_profit = calculate_max_profit(date)
+        max_profit = calculate_max_profit(date, PotatoPrice)
 
         render json: {max_profit: max_profit}
       end
 
       private
 
-      def calculate_max_profit(date)
-        DailyProfitCalculator.new(date, PotatoPrice).call
+      def calculate_max_profit(date, price_model)
+        DailyProfitCalculator.new(date, price_model).call
       end
     end
   end
